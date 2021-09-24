2db02ed1b1e1f768bbbafb8402db5859a25de036
Abhishek Yadav <89199323+abhishekY2401@users.noreply.github.com>
2021-09-24 18:50:08 +0530

    made 2nd changes in file

--- Notes-DS ALGO/Algorithms/Sorting/Bubble Sort/bubbleSort.java
+++ Notes-DS ALGO/Algorithms/Sorting/Bubble Sort/bubbleSort.java
@@ -18,10 +18,10 @@ class bubbleSort {
 			for (int j = 1; j < n-i; j++) {
 				if (arr[j] < arr[j - 1]) {
 
-                    // Swapping elements if the previous element is greater than current element
-                    int temp = arr[j];
-                    arr[j] = arr[j - 1];
-                    arr[j - 1] = temp;
+				    // Swapping elements if the previous element is greater than current element
+				    int temp = arr[j];
+				    arr[j] = arr[j - 1];
+				    arr[j - 1] = temp;
 				}
 			}
 		}
