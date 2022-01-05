c1dd1bedcf004b935077100bb0ffe13ca2804fe6
abhishekY2401 <abhiyd2401@gmail.com>
2022-01-05 14:50:20 -0800

    Updated insertion sort

--- main/Notes-DS-ALGO/Algorithms/Sorting/insertionSort.java
+++ main/Notes-DS-ALGO/Algorithms/Sorting/insertionSort.java
@@ -3,16 +3,20 @@
 *   The approach is to divide the array into two parts
 *   sorted part and the unsorted part
 *
+*    1st iteration
 *   [45,67,12,23,56] -> [45 | 67,12,23,56] -> 45 < 67 so no swap takes place
 *
+*    2nd iteration
 *   [45,67 | 12,23,56] -> 67 > 12. So swap 67 and 12 with each other
 *   [45,12,67 | 23,56] -> when comparing from unsorted with sorted element traverse the whole unsorted part
 *   and compare with the predecessor. -> 45 > 12. So swap 45 and 12 with each other
 *
+*    3rd iteration
 *   [12,45,67 | 23,56] -> 67 > 23. So swap 67 and 23 with each other
 *   [12,45,23,67 | 56] -> 45 > 23. So swap 45 and 23 with each other
 *   [12,23,45,67 | 56] -> 12 < 23 no swap takes place
 *
+*    4th iteration
 *   [12,23,45,67 | 56] -> 67 > 56. So swap 67 and 56 with each other
 *   [12,23,45,56,67] -> 45 < 56 no swap takes place
 *
@@ -32,16 +36,16 @@ class insertionSort {
     private static void insertion(int[] arr) {
         int len = arr.length;
 
-        for (int i = 1; i < len - 1; i++) {
+        for (int i = 1; i < len; i++) {
             int key = arr[i];
             int j = i-1;
 
-            while (j >= 0 && arr[j] > key) {
-                arr[j+1] = arr[j];
-                j--;
+            while (j >= 0 && key < arr[j]) {
+                arr[j + 1] = arr[j];
+                --j;
             }
 
-
+            arr[j+1] = key;
         }
     }
 }
